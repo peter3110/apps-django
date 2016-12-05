db02e8227c4159fda5375ead2b565431883d94a4
peter3110 <peter3110>
2016-12-04 22:54:17 -0300

    adf

--- inflaciometro/encuestas/forms.py
+++ inflaciometro/encuestas/forms.py
@@ -3,13 +3,13 @@ from django import forms
 from .models import datosUsuarioAnonimo
 # our new form
 class datosUsuarioAnonimoForm(forms.ModelForm):
-    ciudad = forms.CharField(max_length=50)
-    edad = forms.IntegerField()
-    expectativaDeInflacion = forms.FloatField()
-    gastoTransporte = forms.IntegerField()
-    gastoEducacion = forms.IntegerField()
-    gastoVivienda = forms.IntegerField()
-    gastoSalud = forms.IntegerField()
+    ciudad = forms.CharField(max_length=50, label="Ciudad")
+    edad = forms.IntegerField(label="Edad")
+    expectativaDeInflacion = forms.FloatField(label="Expectativa de inflacion")
+    gastoTransporte = forms.IntegerField(label="Gasto en Transporte")
+    gastoEducacion = forms.IntegerField(label="Gasto en Educacion")
+    gastoVivienda = forms.IntegerField(label="Gasto en Vivienda")
+    gastoSalud = forms.IntegerField(label="Gasto en Salud")
 
     class Meta:
         model = datosUsuarioAnonimo
--- inflaciometro/encuestas/views.py
+++ inflaciometro/encuestas/views.py
@@ -8,9 +8,31 @@ from .forms import datosUsuarioAnonimoForm
 def index(request):
     return HttpResponse("Hello, world. Esta es la pagina principal de la app 'Encuestas' ")
 
+def make_plot():
+    from bokeh.plotting import figure, output_file, show 
+    from bokeh.embed import components
+
+    x= [1,3,5,7,9,11,13]
+    y= [1,2,3,4,5,6,7]
+    title = 'y = f(x)'
+
+    plot = figure(title= title , 
+        x_axis_label= 'X-Axis', 
+        y_axis_label= 'Y-Axis', 
+        plot_width =400,
+        plot_height =400)
+
+    plot.line(x, y, legend= 'f(x)', line_width = 2)
+    return components(plot)
+
 def graficos(request, context):
-    #return HttpResponse("Aca van los graficos obtenidos a partir de los datos de la form")
-    return render(request, 'graficos/grafico1.html', context)
+    from django.shortcuts import render, render_to_response
+    
+    script, div = make_plot()
+    context['title'] = "Aca van los graficos a partir de los datos: "
+    context['script'] = script
+    context['div'] = div
+    return render_to_response('graficos/grafico1.html', context)
 
 def preguntas(request):
     welcome_str = "Hello, world. Esta es la pagina de preguntas de la app 'Encuestas' "
@@ -26,7 +48,7 @@ def preguntas(request):
 
             # Now call the index() view.
             # The user will be shown the homepage.
-            return graficos(request, {'form': form})
+            return graficos(request, {'data': form.cleaned_data})
         else:
             # The supplied form contained errors - just print them to the terminal.
             print form.errors
